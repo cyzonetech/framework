0d0ff8f6124bc7df813661726633c2fce8ac1b0b
cyzonetech <30819954+cyzonetech@users.noreply.github.com>
2018-04-09 10:10:49 +0800

    file还原
    file还原

--- library/think/template/driver/File.php
+++ library/think/template/driver/File.php
@@ -15,38 +15,19 @@ use think\Exception;
 
 class File
 {
-
-    /**
-     * @var null
-     */
-    private $vars = null;
-
-    /**
-     * @var null
-     */
-    private $config = null;
-
-    /**
-     * @var null
-     */
-    private $engine = null;
-
     /**
      * 写入编译缓存
-     * @access public
-     * @param  string $cacheFile 缓存的文件名
-     * @param  string $content 缓存的内容
+     * @param string $cacheFile 缓存的文件名
+     * @param string $content 缓存的内容
      * @return void|array
      */
     public function write($cacheFile, $content)
     {
         // 检测模板目录
         $dir = dirname($cacheFile);
-
         if (!is_dir($dir)) {
             mkdir($dir, 0755, true);
         }
-
         // 生成模板缓存文件
         if (false === file_put_contents($cacheFile, $content)) {
             throw new Exception('cache write error:' . $cacheFile, 11602);
@@ -55,31 +36,24 @@ class File
 
     /**
      * 读取编译编译
-     * @access public
-     * @param  string  $cacheFile 缓存的文件名
-     * @param  array   $vars 变量数组
+     * @param string  $cacheFile 缓存的文件名
+     * @param array   $vars 变量数组
      * @return void
      */
-    public function read($cacheFile, &$vars = null, &$config = null, &$engine = null)
+    public function read($cacheFile, $vars = [])
     {
-        $this->vars = $vars;
-        $this->config = $config;
-        $this->engine = $engine;
-
         if (!empty($vars) && is_array($vars)) {
             // 模板阵列变量分解成为独立变量
             extract($vars, EXTR_OVERWRITE);
         }
-
         //载入模版缓存文件
         include $cacheFile;
     }
 
     /**
      * 检查编译缓存是否有效
-     * @access public
-     * @param  string  $cacheFile 缓存的文件名
-     * @param  int     $cacheTime 缓存时间
+     * @param string  $cacheFile 缓存的文件名
+     * @param int     $cacheTime 缓存时间
      * @return boolean
      */
     public function check($cacheFile, $cacheTime)
@@ -88,12 +62,10 @@ class File
         if (!file_exists($cacheFile)) {
             return false;
         }
-
-        if (0 != $cacheTime && time() > filemtime($cacheFile) + $cacheTime) {
+        if (0 != $cacheTime && $_SERVER['REQUEST_TIME'] > filemtime($cacheFile) + $cacheTime) {
             // 缓存是否在有效期
             return false;
         }
-
         return true;
     }
 }
